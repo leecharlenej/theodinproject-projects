26e7242b83941426b79f181b2f34c21f63085828
leecharlenej <leecharlenej@gmail.com>
2024-09-12 11:09:35 +0800

    Weather app: Complete.

--- odin-weather-app/script.js
+++ odin-weather-app/script.js
@@ -1,5 +1,3 @@
-let locationInput = 'London'
-
 async function fetchLocationData (locationName) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}?unitGroup=us&key=TBU87BK768UWZGZ43Y6AMMZMT&contentType=json`, {mode:'cors'});
     const locationData = await response.json();
@@ -33,12 +31,6 @@ function getLocationDetails (locationData) {
     return locationObject;
 }
 
-
-function changeTempToC (temperature) {
-    const tempCelsius = (temperature-32) * 5/9
-    return tempCelsius.toFixed(2);
-}
-
 function getLocationInput() {
     const locationInputBox = document.querySelector("#location-input");
     const locationInputValue = locationInputBox.value;
@@ -47,8 +39,9 @@ function getLocationInput() {
 }
 
 async function main() {
-    let locationDisplayInput = document.querySelector("#locationDisplayInput h2");
-    let locationDisplayDate = document.querySelector("#locationDisplayDate");
+    const locationResults = document.querySelector(".location-results");
+    const locationDisplayInput = document.querySelector("#locationDisplayInput h2");
+    const locationDisplayDate = document.querySelector("#locationDisplayDate");
     const locationDisplayTemp = document.querySelector("#locationDisplayTemp");
     const locationDisplayFeelsLike = document.querySelector("#locationDisplayFeelsLike");
     const locationDisplayConditions = document.querySelector("#locationDisplayConditions");
@@ -59,12 +52,15 @@ async function main() {
         const locationData = await fetchLocationData(locationInput);
         const locationObject = getLocationDetails(locationData);
         
+        locationResults.classList.remove("hidden");
+        toggleButton.classList.remove("hidden");
         locationDisplayInput.textContent = locationInput;
         locationDisplayDate.textContent = locationObject.date;
         locationDisplayTemp.textContent = locationObject.temperature;
         locationDisplayFeelsLike.textContent = locationObject.temperatureFeelsLike;
         locationDisplayConditions.textContent = locationObject.conditionDetails;
         locationDisplayHumidity.textContent = locationObject.humidityDetails;
+
         
     }catch (error) {
         console.error(`'Error fetching location data: ' ${error}`);
@@ -76,3 +72,48 @@ async function main() {
 
 const searchButton = document.querySelector('#search-button');
 searchButton.addEventListener("click", main);
+
+
+let tempFahrenheitFlag = true;
+
+function toggleTemp (temperature) {
+
+    let temperatureObject = {
+        "temperature": 0,
+        "buttonMessage": ''
+    }
+
+    if (tempFahrenheitFlag) {
+        const tempCelsius = (temperature-32) * 5/9;
+        temperatureObject.temperature = tempCelsius.toFixed(1);
+        temperatureObject.buttonMessage = 'Change to Fahrenheit';
+        tempFahrenheitFlag = false;
+
+    } else {
+        const tempFahrenheit = (temperature * 9/5) + 32;
+        temperatureObject.temperature = tempFahrenheit.toFixed(1);
+        temperatureObject.buttonMessage = 'Change to Celsius';
+        tempFahrenheitFlag = true;
+    } 
+
+    return temperatureObject;
+}
+
+function changeTempDisplay () {
+    const temperatureObject = (toggleTemp(locationDisplayTemp.textContent));
+    console.log(temperatureObject);
+    locationDisplayTemp.textContent = temperatureObject.temperature;
+    toggleButton.textContent = temperatureObject.buttonMessage;
+
+}
+
+const toggleButton = document.querySelector('#toggle-button');
+toggleButton.addEventListener("click", changeTempDisplay);
+
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener("click", ()=> {
+    const locationResults = document.querySelector(".location-results");
+    locationResults.classList.add("hidden")
+    const locationInput = document.querySelector("#location-input");
+    locationInput.value="";
+});
